cbaf7c39821c20d1a994e7d687d4793e166d629a
hchoHsu <vani900728@gmail.com>
2021-03-26 21:58:00 +0800

    able to draw on canvas

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
 /* main variable */
+var cvs, ctx;   // canvas object
+var mouse = {   // global mouse object
+    x: 0,
+    y: 0,
 
-// canvas object
-var cvs, ctx;
-
-// global mouse object
-var mouse = {
-    x: undefined,
-    y: undefined
+    hold: false     // mouse down or not
 }
 
+/* draw functions */
+function drawLine (x2, y2){
+    ctx.beginPath();
+    ctx.stroleStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.moveTo(mouse.x, mouse.y);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+/* mouse function */
+function mouseMove (event){
+    if (mouse.hold === true) {
+        drawLine(event.offsetX, event.offsetY);
+        mouse.x = event.offsetX;
+        mouse.y = event.offsetY;
+    }
+}
 function mouseDown (event){
-    mouse.x = event.pageX;
-    mouse.y = event.pageY;
+    mouse.x = event.offsetX;
+    mouse.y = event.offsetY;
 
-    console.log(mouse);
+    mouse.hold = true;
+    console.log(mouse); // check where's the mouse
+}
+function mouseUp (event){
+    if (mouse.hold === true) {
+        drawLine(event.offsetX, event.offsetY);
+        mouse.x = 0;
+        mouse.y = 0;
+        mouse.hold = false;
+    }
+    console.log(mouse); // check where's the mouse
 }
 
+/* main function */
 window.onload = function (){
     cvs = document.getElementById("canvas");
     ctx = cvs.getContext("2d");
 
-    // cvs.addListener:
-    // mousemove (detect the mouse moving or not)
-    // mousedown (detect the mouse down)
+    // cvs.addListener: https://developer.mozilla.org/zh-CN/docs/Web/Events
+    cvs.addEventListener('mousemove', mouseMove, false);
     cvs.addEventListener('mousedown', mouseDown, false);
+    cvs.addEventListener('mouseup'  , mouseUp  , false);
 }
