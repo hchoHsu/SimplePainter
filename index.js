427a341a97e6f067f44f5f593f8b843a5ed6d1aa
hchoHsu <vani900728@gmail.com>
2021-03-26 21:24:03 +0800

    add mouse object

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+/* main variable */
+
+// canvas object
 var cvs, ctx;
 
-function mouseDown (event){
-    let cx = event.pageX;
-    let cy = event.pageY;
+// global mouse object
+var mouse = {
+    x: undefined,
+    y: undefined
+}
 
-    console.log('(' + cx + ',' + cy + ')');
+function mouseDown (event){
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
+
+    console.log(mouse);
 }
 
 window.onload = function (){
     cvs = document.getElementById("canvas");
     ctx = cvs.getContext("2d");
 
+    // cvs.addListener:
+    // mousemove (detect the mouse moving or not)
+    // mousedown (detect the mouse down)
     cvs.addEventListener('mousedown', mouseDown, false);
 }
